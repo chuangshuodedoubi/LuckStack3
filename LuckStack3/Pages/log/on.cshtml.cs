664993fd512521ee972cc478f593ae29dcc27c7c
dou_bi <1550760192@qq.com>
2020-07-27 22:08:27 +0800

    添加一个table,上面横向合并单元格，中间左边纵向合并3个单元个，右侧3单元格，底部横向合并单元格

---
+++ LuckStack3/Pages/log/on.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LuckStack3.Pages.log
+{
+    public class onModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
